14e0507f9aeb35f8aaa2e3509abb2bbad986e6d4
JudasII <juan.david.208080@gmail.com>
2023-12-13 16:40:54 -0500

    implementing pagination and sorting

--- src/main/java/com/example/cashCard/repositories/CashCardRepository.java
+++ src/main/java/com/example/cashCard/repositories/CashCardRepository.java
@@ -3,6 +3,7 @@ package com.example.cashCard.repositories;
 
 import com.example.cashCard.models.CashCard;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>,PagingAndSortingRepository<CashCard, Long> {
 }
