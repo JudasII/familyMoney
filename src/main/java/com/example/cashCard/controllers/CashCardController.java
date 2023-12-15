11e72e642b8e6fa5cf885eedb80c8517c2fee159
JudasII <juan.david.208080@gmail.com>
2023-12-15 16:35:56 -0500

    implementing delete

--- src/main/java/com/example/cashCard/controllers/CashCardController.java
+++ src/main/java/com/example/cashCard/controllers/CashCardController.java
@@ -63,4 +63,14 @@ class CashCardController {
             return ResponseEntity.noContent().build();
         }).orElseGet(() -> ResponseEntity.notFound().build());
     }
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteCashCard(@PathVariable Long id, Principal principal) {
+
+        if (!cashCardRepository.existsByIdAndOwner(id, principal.getName())) {
+            return ResponseEntity.notFound().build();
+        }
+        cashCardRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
